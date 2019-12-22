4ef6b6ef205b6fd1b7269407582939fb159dd542
Aaryan Dewan <49852384+aaryandewan@users.noreply.github.com>
2019-12-22 19:18:39 +0530

    Added the entire scrapping functionality

--- webscrapper/webapp/views.py
+++ webscrapper/webapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+import requests
+from .models import Link
+from bs4 import BeautifulSoup
 
-# Create your views here.
+def scrape(request):
+    if request.method == 'POST':
+        site = request.POST.get('site', '')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        for link in soup.find_all('a'):
+            link_addresses = link.get('href')
+            link_text = link.string
+            Link.objects.create(address = link_addresses, name = link_text)
+            #link_addresses.append(link.get('href'))
+        return HttpResponseRedirect('/')
+    else:
+        data = Link.objects.all()
+
+    return render(request, 'webapp/resut.html', {'data': data})
--- webscrapper/webscrapper/urls.py
+++ webscrapper/webscrapper/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views
+
 
 urlpatterns = [
+    path('', views.scrape, name = "scrape"),
     path('admin/', admin.site.urls),
 ]
